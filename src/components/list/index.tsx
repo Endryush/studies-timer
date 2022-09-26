0a941545bf632c4a73dca8193d4b532ffd6ea84e
Endryush <endryush@gmail.com>
2022-09-26 20:19:49 -0300

    Init to control selected tasks

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -2,16 +2,20 @@ import { ITarefa } from "../../types/tarefa";
 import Item from "./Item";
 import style from './List.module.scss';
 
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
+}
 
-
-function List({ tarefas }: { tarefas : ITarefa[]}) {
+function List({ tarefas, selecionaTarefa }: Props) {
   return (
     <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item 
-            key={index}
+            key={item.id}
+            selecionaTarefa={selecionaTarefa}
             {...item}
           />
         ))}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,27 +2,23 @@ import React, { useState } from "react";
 import Cronometro from '../components/Cronometro';
 import Form from '../components/form';
 import List from '../components/list';
+import { ITarefa } from "../types/tarefa";
 import style from './App.module.scss';
 
 function App() {
-  const [tarefas, setTarefas] = useState([
-    {
-      tarefa: "React",
-      tempo: " 02:00:00",
-    },
-    {
-      tarefa: "JS",
-      tempo: " 01:00:00",
-    },
-    {
-      tarefa: "TS",
-      tempo: " 01:30:00",
-    }
-  ])
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const[selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada : ITarefa) {
+    setSelecionado(tarefaSelecionada)
+  }
   return (
     <div className={style.AppStyle}>
       <Form setTarefas={setTarefas} />
-      <List tarefas={tarefas} />
+      <List 
+        tarefas={tarefas} 
+        selecionaTarefa={selecionaTarefa} 
+      />
       <Cronometro />
     </div>
   );
